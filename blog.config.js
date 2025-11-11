07f4a347c36f4d6f390537eabb3f879a6e9938a2
cha310 <lmcha528@gmail.com>
2025-11-11 11:39:21 +0800

    网站

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '阿叉', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '推动世界变得更好的起点，是让自己先变得更好。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://achacha.cn', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://achatalk.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'cha, achacha, acha, blog, Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
